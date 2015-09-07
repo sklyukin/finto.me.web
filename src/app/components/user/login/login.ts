96ff93b9bc4638e36e75ebc093e753c0de4b4fcf
sklyukin <stas.msu@gmail.com>
2015-09-08 00:55:52 +0500

    login page styling

--- src/app/components/user/login/login.ts
+++ src/app/components/user/login/login.ts
@@ -1,33 +1,22 @@
 /// <reference path="../../../../typings/_custom.d.ts" />
 
-/*
- * Angular 2
- */
 import {Component, View, ViewEncapsulation, ControlGroup} from 'angular2/angular2';
 import {FormBuilder, Validators} from 'angular2/forms';
 import {UserService} from 'app/services/UserService'
 
-/*
- * Directives
- * angularDirectives: Angular's core/form/router directives
- * appDirectives: Our collection of directives from /directives
- */
 import {appDirectives, angularDirectives} from 'app/directives/directives';
 
-// Use webpack's `require` to get files as a raw string using raw-loader
-//let styles = require('./login.css');
+let style = require('./login.scss');
 let template = require('./login.html');
 
-// Simple external file component example
 @Component({
   selector: 'login',
 })
 @View({
   directives: [angularDirectives, appDirectives],
   encapsulation: ViewEncapsulation.EMULATED,
-  // include our .html and .css file
-  //styles: [styles],
-  template: template
+  template: template,
+  styles: [style]
 })
 export class Login {
   loginForm:ControlGroup;
